97f3a25493acba167741dd3ed16cd297646f4b4f
mutasim99 <virginmr882@gmail.com>
2026-08-31 10:39:53 -0700

    create jwt token

---
+++ src/app/utils/token.ts
@@ -0,0 +1,23 @@
+import { JwtPayload, SignOptions } from "jsonwebtoken";
+import { jwtUtils } from "./jwt";
+import { envVars } from "../config/env";
+
+const getAccessToken = (payload: JwtPayload) => {
+  const accessToken = jwtUtils.createToken(
+    payload,
+    envVars.ACCESS_TOKEN_SECRET,
+    { expiresIn: envVars.ACCESS_TOKEN_EXPIRE_IN } as SignOptions,
+  );
+};
+const getRefreshToken = (payload: JwtPayload) => {
+  const accessToken = jwtUtils.createToken(
+    payload,
+    envVars.REFRESH_TOKEN_SECRET,
+    { expiresIn: envVars.REFRESH_TOKEN_EXPIRE_IN } as SignOptions,
+  );
+};
+
+export const tokenUtils = {
+  getAccessToken,
+  getRefreshToken,
+};
